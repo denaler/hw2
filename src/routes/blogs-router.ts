e990e009437c6123a01fc9e3ed74c3bc6764acbd
Daler <kuperr9@gmail.com>
2023-10-23 20:31:25 +0700

    init commit

--- src/routes/blogs-router.ts
+++ src/routes/blogs-router.ts
@@ -13,31 +13,121 @@ type BlogType = {
     websiteUrl: string
 }
 
-export const blogsDb: BlogType[] = [
-    {
-        id: "1",
-        name: "daler",
-        description: "string",
-        websiteUrl: "https://localhost:4343"
+type ErrorsMessages = {
+    message: string,
+    field: string
+}
+
+type ErrorType = {
+    errorsMessages: ErrorsMessages[]
+}
+export const blogsDb: BlogType[] = []
+
+blogsRouter.get('/', (req: Request, res: Response) => {
+    res.status(200).send(blogsDb)
+})
+
+blogsRouter.post('/', (req: RequestWithBody<
+    {name: string,
+    description: string,
+    websiteUrl: string
+    }>, res: Response) => {
+
+    let errors: ErrorType = {
+        errorsMessages: []
     }
-]
+    let {name, description, websiteUrl} = req.body
 
-blogsRouter.get('/', (req:Request, res: Response) => {
+    if (!name || name.trim().length > 15) {
+        errors.errorsMessages.push({message: 'Invalid name', field: 'name'})
+    }
+    if (!description || description.trim().length > 500) {
+        errors.errorsMessages.push({message: 'Invalid description', field: 'description'})
+    }
+    if (!websiteUrl || websiteUrl.trim().length > 100) {
+        errors.errorsMessages.push({message: 'Invalid websiteUrl', field: 'websiteUrl'})
+    }
 
+    if (errors.errorsMessages.length) {
+        res.status(400).send(errors)
+        return
+    }
+
+    const newBlog: BlogType = {
+        id: (new Date()).toString(),
+        name: name,
+        description: description,
+        websiteUrl: websiteUrl
+    }
+
+    blogsDb.push(newBlog)
+
+    res.status(201).send(newBlog)
 })
 
-blogsRouter.post('/', (req:Request, res: Response) => {
+blogsRouter.get('/:id', (req:RequestWithParams<{ id: string }>, res: Response) => {
+    const id = req.params.id
+    const blog = blogsDb.find((blog) => blog.id === id)
 
+    if (!blog) {
+        res.sendStatus(404)
+        return
+    }
+    res.status(200).send(blog)
 })
 
-blogsRouter.get('/:id', (req:Request, res: Response) => {
+blogsRouter.put('/:id', (req:RequestWithParamsAndBody<
+    { id: string },
+    { name: string,
+    description: string,
+    websiteUrl: string
+}>, res: Response) => {
 
+    const id = req.params.id
+    const blog = blogsDb.find((blog) => blog.id === id)
+
+    if (!blog) {
+        res.sendStatus(404)
+        return
+    }
+
+    let errors: ErrorType = {
+        errorsMessages: []
+    }
+    let {name, description, websiteUrl} = req.body
+
+    if (!name || name.trim().length > 15) {
+        errors.errorsMessages.push({message: 'Invalid name', field: 'name'})
+    }
+    if (!description || description.trim().length > 500) {
+        errors.errorsMessages.push({message: 'Invalid description', field: 'description'})
+    }
+    if (!websiteUrl || websiteUrl.trim().length > 100) {
+        errors.errorsMessages.push({message: 'Invalid websiteUrl', field: 'websiteUrl'})
+    }
+
+    if (errors.errorsMessages.length) {
+        res.status(400).send(errors)
+        return
+    }
+
+    let i = blogsDb.indexOf(blog)
+    blogsDb[i].name = name
+    blogsDb[i].description = description
+    blogsDb[i].websiteUrl = websiteUrl
+
+    res.sendStatus(204)
 })
 
-blogsRouter.put('/:id', (req:Request, res: Response) => {
+blogsRouter.delete('/:id', (req:RequestWithParams<{ id: string }>, res: Response) => {
+    const id = req.params.id
+    const blog = blogsDb.find((blog) => blog.id === id)
 
-})
+    if (!blog) {
+        res.sendStatus(404)
+        return
+    }
+    blogsDb.slice(blogsDb.indexOf(blog), 1)
 
-blogsRouter.delete('/:id', (req:Request, res: Response) => {
-
+    res.sendStatus(204)
 })
--- src/routes/posts-router.ts
+++ src/routes/posts-router.ts
@@ -1,6 +1,5 @@
 import {Router} from "express";
 import express, {Request, Response} from 'express';
-import {blogsRouter} from "./blogs-router";
 export const postsRouter = Router({})
 
 type RequestWithParams<P> = Request<P, {}, {}, {}>
@@ -16,33 +15,133 @@ type PostType = {
     blogName: string
 }
 
-export const postsDb: PostType[] = [
-    {
-        id: "0",
-        title: "hello",
-        shortDescription: "si",
-        content: "back",
-        blogId: "1",
-        blogName: "dalerback"
-    }
-]
+type ErrorsMessages = {
+    message: string,
+    field: string
+}
+
+type ErrorType = {
+    errorsMessages: ErrorsMessages[]
+}
+
+export const postsDb: PostType[] = []
 
 postsRouter.get('/', (req:Request, res: Response) => {
-
+    res.status(200).send(postsDb)
 })
 
-postsRouter.post('/', (req:Request, res: Response) => {
+postsRouter.post('/', (req:RequestWithBody<
+    {title: string,
+    shortDescription: string,
+    content: string,
+    blogId: string
+    }>, res: Response) => {
 
+    let errors: ErrorType = {
+        errorsMessages: []
+    }
+    let {title, shortDescription, content, blogId} = req.body
+
+    if (!title || title.trim().length > 30) {
+        errors.errorsMessages.push({message: 'Invalid title', field: 'title'})
+    }
+    if (!shortDescription || shortDescription.trim().length > 500) {
+        errors.errorsMessages.push({message: 'Invalid shortDescription', field: 'shortDescription'})
+    }
+    if (!content || content.trim().length > 1000) {
+        errors.errorsMessages.push({message: 'Invalid content', field: 'content'})
+    }
+    if (!blogId) {
+        errors.errorsMessages.push({message: 'Invalid blogId', field: 'blogId'})
+    }
+
+    if (errors.errorsMessages.length) {
+        res.status(400).send(errors)
+        return
+    }
+
+    const newPost: PostType = {
+        id: (new Date()).toString(),
+        title: title,
+        shortDescription: shortDescription,
+        content: content,
+        blogId: blogId,
+        blogName: ''
+    }
+
+    postsDb.push(newPost)
+
+    res.status(201).send(newPost)
 })
 
-postsRouter.get('/:id', (req:Request, res: Response) => {
+postsRouter.get('/:id', (req:RequestWithParams<{ id: string }>, res: Response) => {
+    const id = req.params.id
+    const post = postsDb.find((post) => post.id === id)
 
+    if (!post) {
+        res.sendStatus(404)
+        return
+    }
+    res.status(200).send(post)
 })
 
-postsRouter.put('/:id', (req:Request, res: Response) => {
+postsRouter.put('/:id', (req:RequestWithParamsAndBody<
+    { id: string },
+    { title: string,
+    shortDescription: string,
+    content: string,
+    blogId: string
+}>, res: Response) => {
 
+    const id = req.params.id
+    const post = postsDb.find((post) => post.id === id)
+
+    if (!post) {
+        res.sendStatus(404)
+        return
+    }
+
+    let errors: ErrorType = {
+        errorsMessages: []
+    }
+    let {title, shortDescription, content, blogId} = req.body
+
+    if (!title || title.trim().length > 30) {
+        errors.errorsMessages.push({message: 'Invalid title', field: 'title'})
+    }
+    if (!shortDescription || shortDescription.trim().length > 100) {
+        errors.errorsMessages.push({message: 'Invalid shortDescription', field: 'shortDescription'})
+    }
+    if (!content || content.trim().length > 1000) {
+        errors.errorsMessages.push({message: 'Invalid content', field: 'content'})
+    }
+    if (!blogId) {
+        errors.errorsMessages.push({message: 'Invalid blogId', field: 'blogId'})
+    }
+
+    if (errors.errorsMessages.length) {
+        res.status(400).send(errors)
+        return
+    }
+
+    let i = postsDb.indexOf(post)
+    postsDb[i].title = title
+    postsDb[i].shortDescription = shortDescription
+    postsDb[i].content = content
+    postsDb[i].blogId = blogId
+
+    res.sendStatus(204)
 })
 
-postsRouter.delete('/:id', (req:Request, res: Response) => {
+postsRouter.delete('/:id', (req:RequestWithParams<{ id: string }>, res: Response) => {
+    const id = req.params.id
+    const post = postsDb.find((post) => post.id === id)
 
+    if (!post) {
+        res.sendStatus(404)
+        return
+    }
+    postsDb.slice(postsDb.indexOf(post), 1)
+
+    res.sendStatus(200)
 })
