bed11614e2f6778eb90f10d2706f6e05e735dcba
Daler <kuperr9@gmail.com>
2023-10-25 17:55:11 +0700

    init commit

--- src/settings.ts
+++ src/settings.ts
@@ -1,12 +1,10 @@
-import express, {Request, Response} from 'express';
+import express from 'express';
 import {blogsRouter} from "./routes/blogs-router";
 import {postsRouter} from "./routes/posts-router";
 import {testingRouter} from "./routes/testing-router";
-
 export const app = express()
 
 app.use(express.json())
-
 app.use('/blogs', blogsRouter)
 app.use('/posts', postsRouter)
 app.use('/testing', testingRouter)
