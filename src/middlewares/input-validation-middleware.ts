b92dfc8a0e910cd67ab690ebb50a98cc6e4596c1
Daler <kuperr9@gmail.com>
2023-10-27 04:12:44 +0700

    init commit

--- src/middlewares/input-validation-middleware.ts
+++ src/middlewares/input-validation-middleware.ts
@@ -5,7 +5,7 @@ import {validationResult} from "express-validator";
 export const inputValidationMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-        res.status(400).json({ errors: errors.array() })
+        res.status(400).json({ errors: errors.array({ onlyFirstError: true }) })
         return
     } else {
         next()
